4c4e62f661907b7d7092507f7910732a238b4b58
Dudewithoutname <54103088+Dudewithoutname@users.noreply.github.com>
2024-01-13 00:47:40 +0100

    fix typo

--- modelmerger/merger.bat
+++ modelmerger/merger.bat
@@ -4,14 +4,14 @@
 :: nezabudni si v .qc subore cd na lokaciu output zlozky
 @echo off
 
-:: ak mas predgenerovanu uz .qc file tak nastava na 2
+:: ak mas predgenerovanu uz .qc file tak nastav na 2 ak nemas tak na vo vysledku aj tak budes muset vytvorit subor .qc manualne s bodygroup partmy
 set /a qcGenerated=2
 :: nazov suboru
 set type=player
 :: cesta 
 :: ntb set jebe=C:\Users\dudew\Desktop\epiczoneskins\EpiczonSkinSystem\modelmerger\%type%
 set path=C:\Users\Správca\Desktop\ctx\ActiveProjects\EpiczonSkinSystem\modelmerger\%type%
-:: cast modelu na (.smd) mergnutie moj napad bol mergnut rovnaky model (rovnake .smd) s inymi textures, ak chces mergnut modely s inak pomenovanymi .smd subormy
+:: cast modelu na (.smd) mergnutie moj napad bol mergnut rovnaky model (rovnake .smd) s inymi textures, ak chces mergnut modely s inak pomenovanymi .smd subormi
 :: tak toto je jedno jak nastavis iba v list permennej nastavis mena .smd suborov na mergnutie kedze toto sluzi skor na premenovanie toho isteho .smd 
 set smd=sas
 :: nazvy modelov (.smd) ktore chces mergnut (treba zmenene textures premenovat ak je to rovnaky model cez Half Life Asset Manager alebo Milkshape)
